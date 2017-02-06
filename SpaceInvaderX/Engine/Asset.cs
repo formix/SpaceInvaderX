aa7b5d740f37271f9e83a246380775ab7a02c044
formix <jpg@formix.org>
2017-02-06 12:58:57 -0500

    added Vector3 reference

--- SpaceInvaderX/Engine/Asset.cs
+++ SpaceInvaderX/Engine/Asset.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,23 +11,58 @@ namespace SpaceInvaderX.Engine
 {
     public abstract class Asset : IDisposable
     {
-        public Stage Stage { get; private set; }
-        public float X { get; set; }
-        public float Y { get; set; }
-        public float Z { get; set; }
-        public bool IsDisposed { get; private set; }
-
+        public Vector3 Position;
 
         public Asset(Stage stage)
         {
             Stage = stage;
             IsDisposed = false;
+            Position = Vector3.Zero;
         }
 
         ~Asset()
         {
             Dispose(false);
         }
+
+        public Stage Stage { get; private set; }
+        public bool IsDisposed { get; private set; }
+
+        public float X {
+            get
+            {
+                return Position.X;
+            }
+            set
+            {
+                Position.X = value;
+            }
+        }
+
+        public float Y
+        {
+            get
+            {
+                return Position.Y;
+            }
+            set
+            {
+                Position.Y = value;
+            }
+        }
+
+        public float Z
+        {
+            get
+            {
+                return Position.Z;
+            }
+            set
+            {
+                Position.Z = value;
+            }
+        }
+
 
         public virtual void Animate()
         {
